143e6ef7915bf687328abd02eb62884900ebcc3f
Cao, Jeffrey <Jeffrey.Cao@grainger.ca>
2025-02-15 20:47:58 -0500

    added api key grab

--- SMTP_setup.py
+++ SMTP_setup.py
@@ -24,7 +24,7 @@ def sfConn():
 
 def getEmail():
 	racfid = getuser().upper()
-	query = f"""SELECT WORK_EMAIL FROM PUBLISH.GSCCE.EMPLOYEE_EDV WHERE USERNAME= '{racfid}'"""
+	query = f"""SELECT WORK_EMAIL FROM PUBLISH.GSCCE.EMPLOYEE_EDV WHERE USERNAME = '{racfid}'"""
 	df = pd.read_sql(query, sfConn())
 	email = df['WORK_EMAIL'].iloc[0]
 
@@ -32,8 +32,20 @@ def getEmail():
 		text_file.write(email)
 
 
+def getSecret():
+    query = """SELECT TO_VARCHAR(DECRYPT(API_KEY, '-sAlly$N3verLies!'), 'UTF-8') AS API_KEY FROM ISP.RA.SMTP_SECRET"""
+    df = pd.read_sql(query, sfConn())
+    api_key = df['API_KEY'].iloc[0]
+
+    with open('DEVLOPER_FILES/api_key.txt', 'w') as text_file:
+        text_file.write(api_key)
+
+
 if not path.exists('user_email.txt'):
       getEmail()
+
+if not path.exists('DEVLOPER_FILES/api_key.txt'):
+      getSecret()
 
 if not path.exists('ATTACHMENT'):
     makedirs('ATTACHMENT')
